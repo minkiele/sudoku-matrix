882be93057864ba777150c2a7beb5131f1116f5e
Michele Montagner <2039317+minkiele@users.noreply.github.com>
2020-10-30 15:32:34 +0100

    Forgot to throw errors when validation does not occur

--- src/lib/validation/ValidationTuple.ts
+++ src/lib/validation/ValidationTuple.ts
@@ -1,9 +1,11 @@
+import { ValidationError } from './ValidationError';
+
 export class ValidationTuple implements Validator {
     public constructor(private tuple: ValidationSchema.Tuple) {}
     public isValid(): boolean {
         for(let i = 0; i < this.tuple.length; i += 1) {
-            if(!this.tuple.some(curr => curr.value === i)) {
-                return false;
+            if(!this.tuple.some(curr => curr.value === i + 1)) {
+                throw new Error(`Missing Value ${i + 1} in a tuple`);
             }
         }
         return true;
